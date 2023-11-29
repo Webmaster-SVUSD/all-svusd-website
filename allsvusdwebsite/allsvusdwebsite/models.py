b0cd498ae50f0d425da89b4d030b180089640943
anushayelanji <anusha.yelanji@gmail.com>
2023-11-29 11:17:30 -0800

    updated events

--- allsvusdwebsite/allsvusdwebsite/models.py
+++ allsvusdwebsite/allsvusdwebsite/models.py
@@ -18,11 +18,18 @@ class QuickLinkCardInfo(CMSPlugin):
     text_body = models.CharField(max_length=500, default='text body')
 
 class EventCardInfo(CMSPlugin):
-    image = models.ImageField(null = True, upload_to='quick_link_images')
-    title = models.CharField(max_length=50, default='Title')
-    date = models.CharField(max_length=50, default='00/00/0000')
-    time = models.CharField(max_length=50, default='00:00')
-    link = models.TextField(max_length=5000, default='Link to Form')
+    image = models.ImageField(null = True, upload_to='quick_link_images', blank=True)
+    title = models.CharField(max_length=500, default='Title')
+    date = models.CharField(max_length=500, default='')
+    time = models.CharField(max_length=500, default='')
+    link1_button_info = models.CharField(max_length=500, default='', blank=True)
+    link_1 = models.TextField(max_length=5000, default="", blank=True)
+    link2_button_info = models.CharField(max_length=500, default='', blank=True)
+    link_2 = models.TextField(max_length=5000, default="", blank=True)
+    link3_button_info = models.CharField(max_length=500, default='', blank=True)
+    link_3 = models.TextField(max_length=5000, default="", blank=True)
+    link4_button_info = models.CharField(max_length=500, default='', blank=True)
+    link_4 = models.TextField(max_length=5000, default="", blank=True)
     text_body = models.TextField(max_length=5000, default='Text Body')
  
 
